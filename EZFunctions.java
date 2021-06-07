ba1c15770153acacdfc920e01ee9badd02c2487f
CreepyChameleon <brendanalm3@gmail.com>
2021-06-07 11:50:38 -0400

    finishing conditionals

--- EZFunctions.java
+++ EZFunctions.java
@@ -12,86 +12,119 @@ public class EZFunctions {
         return newString;
     }
 
-    public static boolean ifCondition(ArrayList<String> wordList){
-        //if the variable VAR1 is equal to the variable VAR2
-        if(wordList.get(2).toLowerCase().equals("variable")){ //variable comparison
-            String obj1Val = Interpreter.globalVariables.get(wordList.get(3))[1];
-            String obj1Type = Interpreter.globalVariables.get(wordList.get(3))[0];
+    public static boolean ifCondition(ArrayList<String> wordList) throws Exception{
+        try{
+            //if the variable VAR1 is equal to the variable VAR2
+            if(wordList.get(2).toLowerCase().equals("variable")){ //variable comparison
+                String obj1Val = Interpreter.globalVariables.get(wordList.get(3))[1];
+                String obj1Type = Interpreter.globalVariables.get(wordList.get(3))[0];
 
-            if(wordList.get(8).toLowerCase().equals("variable")){ //var to var
-                String obj2Val = Interpreter.globalVariables.get(wordList.get(9))[1];
-                String obj2Type = Interpreter.globalVariables.get(wordList.get(9))[0];
-            }
-            else if(wordList.get(8).toLowerCase().equals("string")){
-                if(obj1Type.equals("string")){
-                    String val2 = wordList.get(9);
-                    if(obj1Val.equals(val2)){
-                        return true;
-                    }
-                    else{
-                        return false;
-                    }
-                }
-                else{
-                    ;//raise type error
-                }
-            }
-            else if(wordList.get(8).toLowerCase().equals("integer")){
-                if(obj1Type.equals("integer")){
-                    int val2 = Integer.parseInt(wordList.get(9));
-                    if(Integer.parseInt(obj1Val) == val2){
-                        return true;
-                    }
-                    else{
-                        return false;
-                    }
-                }
-                else{
-                    ;//raise type error
-                }
-            }
-            else if(wordList.get(8).toLowerCase().equals("double")){
-                if(obj1Type.equals("double")){
-                    double val2 = Double.parseDouble(wordList.get(9));
-                    if(Double.parseDouble(obj1Val) == val2){
-                        return true;
-                    }
-                    else{
-                        return false;
-                    }
-                }
-                else{
-                    ;//raise type error
-                }
-            }
-            else if(wordList.get(8).toLowerCase().equals("boolean")){
-                if(obj1Type.equals("boolean")){
-                    boolean val2 = Boolean.parseBoolean(wordList.get(9));
-                    if(Boolean.parseBoolean(obj1Val) == val2){
-                        return true;
-                    }
-                    else{
-                        return false;
-                    }
-                }
-                else{
-                    ;//raise type error
-                }
-            }
-            else{
-                ;//raise type error
-            }
+                if(wordList.get(8).toLowerCase().equals("variable")){ //var to var
+                    String obj2Val = Interpreter.globalVariables.get(wordList.get(9))[1];
+                    String obj2Type = Interpreter.globalVariables.get(wordList.get(9))[0];
 
-
-            
-        }
-        else if(wordList.get(2).toLowerCase().equals("string")){
-            String obj1Val = wordList.get(3);
-            if(wordList.get(8).toLowerCase().equals("variable")){
-                String varVal = Interpreter.globalVariables.get(wordList.get(9))[1];
-                String varType = Interpreter.globalVariables.get(wordList.get(9))[0];
-                if(varType.equals("string")){
-                    if(obj1Val.equals(varVal)){
+                    //check if types are the same
+                    if(obj1Type.equals(obj2Type)){
+                        if(obj1Type.equals("string")){
+                            if(obj1Val.equals(obj2Val)){
+                                return true;
+                            }
+                            else{
+                                return false;
+                            }
+                        }
+                        else{
+                            if(obj1Val == obj2Val){
+                                return true;
+                            }
+                            else{
+                                return false;
+                            }
+                        }
+                    }
+                    else{
+                        throw new Exception("Type Error");
+                    }
+                }
+                else if(wordList.get(8).toLowerCase().equals("string")){
+                    if(obj1Type.equals("string")){
+                        String val2 = wordList.get(9);
+                        if(obj1Val.equals(val2)){
+                            return true;
+                        }
+                        else{
+                            return false;
+                        }
+                    }
+                    else{
+                        throw new Exception("Type Error");
+                    }
+                }
+                else if(wordList.get(8).toLowerCase().equals("integer")){
+                    if(obj1Type.equals("integer")){
+                        int val2 = Integer.parseInt(wordList.get(9));
+                        if(Integer.parseInt(obj1Val) == val2){
+                            return true;
+                        }
+                        else{
+                            return false;
+                        }
+                    }
+                    else{
+                        throw new Exception("Type Error");
+                    }
+                }
+                else if(wordList.get(8).toLowerCase().equals("double")){
+                    if(obj1Type.equals("double")){
+                        double val2 = Double.parseDouble(wordList.get(9));
+                        if(Double.parseDouble(obj1Val) == val2){
+                            return true;
+                        }
+                        else{
+                            return false;
+                        }
+                    }
+                    else{
+                        throw new Exception("Type Error");
+                    }
+                }
+                else if(wordList.get(8).toLowerCase().equals("boolean")){
+                    if(obj1Type.equals("boolean")){
+                        boolean val2 = Boolean.parseBoolean(wordList.get(9));
+                        if(Boolean.parseBoolean(obj1Val) == val2){
+                            return true;
+                        }
+                        else{
+                            return false;
+                        }
+                    }
+                    else{
+                        throw new Exception("Type Error");
+                    }
+                }
+                else{
+                    throw new Exception("Type Error");
+                }  
+            }
+            else if(wordList.get(2).toLowerCase().equals("string")){
+                String obj1Val = wordList.get(3);
+                if(wordList.get(8).toLowerCase().equals("variable")){
+                    String varVal = Interpreter.globalVariables.get(wordList.get(9))[1];
+                    String varType = Interpreter.globalVariables.get(wordList.get(9))[0];
+                    if(varType.equals("string")){
+                        if(obj1Val.equals(varVal)){
+                            return true;
+                        }
+                        else{
+                            return false;
+                        }
+                    }
+                    else{
+                        throw new Exception("Type Error");
+                    }
+                }
+                else if(wordList.get(8).toLowerCase().equals("string")){
+                    if(obj1Val.equals(wordList.get(9))){
                         return true;
                     }
                     else{
@@ -99,28 +132,28 @@ public class EZFunctions {
                     }
                 }
                 else{
-                    ;//raise type error
+                    throw new Exception("Type Error");
                 }
             }
-            else if(wordList.get(8).toLowerCase().equals("string")){
-                if(obj1Val.equals(wordList.get(9))){
-                    return true;
+            else if(wordList.get(2).toLowerCase().equals("integer")){
+                int obj1Val = Integer.parseInt(wordList.get(3));
+                if(wordList.get(8).toLowerCase().equals("variable")){
+                    int varVal = Integer.parseInt(Interpreter.globalVariables.get(wordList.get(9))[1]);
+                    String varType = Interpreter.globalVariables.get(wordList.get(9))[0];
+                    if(varType.equals("integer")){
+                        if(obj1Val == varVal){
+                            return true;
+                        }
+                        else{
+                            return false;
+                        }
+                    }
+                    else{
+                        throw new Exception("Type Error");
+                    }
                 }
-                else{
-                    return false;
-                }
-            }
-            else{
-                ;//raise type error
-            }
-        }
-        else if(wordList.get(2).toLowerCase().equals("integer")){
-            int obj1Val = Integer.parseInt(wordList.get(3));
-            if(wordList.get(8).toLowerCase().equals("variable")){
-                int varVal = Integer.parseInt(Interpreter.globalVariables.get(wordList.get(9))[1]);
-                String varType = Interpreter.globalVariables.get(wordList.get(9))[0];
-                if(varType.equals("integer")){
-                    if(obj1Val == varVal){
+                else if(wordList.get(8).toLowerCase().equals("integer")){
+                    if(obj1Val == Integer.parseInt(wordList.get(9))){
                         return true;
                     }
                     else{
@@ -128,27 +161,72 @@ public class EZFunctions {
                     }
                 }
                 else{
-                    ;//raise type error
+                    throw new Exception("Type Error");
                 }
             }
-            else if(wordList.get(8).toLowerCase().equals("integer")){
-                if(obj1Val == Integer.parseInt(wordList.get(9))){
-                    return true;
+            else if(wordList.get(2).toLowerCase().equals("double")){
+                double obj1Val = Double.parseDouble(wordList.get(3));
+                if(wordList.get(8).toLowerCase().equals("variable")){
+                    double varVal = Double.parseDouble(Interpreter.globalVariables.get(wordList.get(9))[1]);
+                    String varType = Interpreter.globalVariables.get(wordList.get(9))[0];
+                    if(varType.equals("double")){
+                        if(obj1Val == varVal){
+                            return true;
+                        }
+                        else{
+                            return false;
+                        }
+                    }
+                    else{
+                        throw new Exception("Type Error");
+                    }
+                }
+                else if(wordList.get(8).toLowerCase().equals("double")){
+                    if(obj1Val == Double.parseDouble(wordList.get(9))){
+                        return true;
+                    }
+                    else{
+                        return false;
+                    }
                 }
                 else{
-                    return false;
+                    throw new Exception("Type Error");
                 }
             }
-            else{
-                ;//raise type error
+            else if(wordList.get(2).toLowerCase().equals("boolean")){
+                boolean obj1Val = Boolean.parseBoolean(wordList.get(3));
+                if(wordList.get(8).toLowerCase().equals("variable")){
+                    boolean varVal = Boolean.parseBoolean(Interpreter.globalVariables.get(wordList.get(9))[1]);
+                    String varType = Interpreter.globalVariables.get(wordList.get(9))[0];
+                    if(varType.equals("boolean")){
+                        if(obj1Val == varVal){
+                            return true;
+                        }
+                        else{
+                            return false;
+                        }
+                    }
+                    else{
+                        throw new Exception("Type Error");
+                    }
+                }
+                else if(wordList.get(8).toLowerCase().equals("boolean")){
+                    if(obj1Val == Boolean.parseBoolean(wordList.get(9))){
+                        return true;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    throw new Exception("Type Error");
+                }
             }
+            return false;
         }
-        else if(wordList.get(2).toLowerCase().equals("double")){
-
+        catch(Exception e){
+            e.printStackTrace();
+            return false;
         }
-        else if(wordList.get(2).toLowerCase().equals("boolean")){
-
-        }
-        return false;
     }
 }
